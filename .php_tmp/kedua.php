4ee2ee04f62230858e0506a09430905e44292413
aerthaze <sampahselokan043@gmail.com>
2026-08-28 15:36:00 +0700

    Praktikum 2C - Function Sederhana

---
+++ .php_tmp/kedua.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function luasLingkaran($jari) : float {
+    $luas = 3.14*$jari*$jari;
+    return $luas;
+}
+function kelilinglingkaran($jari) : float {
+    $keliling = 2*$jari*3.14;
+    return $keliling;
+}
+function volumebola($jari) : float {
+    $volume = 4/3*3.14*$jari*$jari*$jari;
+    return $volume;
+}
+function volumetabung($jari, $tinggi) : float {
+    $volume = 3.14*$jari*$jari*$tinggi;
+    return $volume;
+}
+function volumekerucut($jari, $tinggi) : float {
+    $volume = 1/3*3.14*$jari*$jari*$tinggi;
+    return $volume;
+}
+$tinggi = 10;
+$jari = 45;
+$luas_tanah = luasLingkaran($jari);
+echo "Luas tanah budi adalah {$luas_tanah}";
+$keliling_tanah = kelilinglingkaran($jari);
+echo "Keliling tanah budi adalah {$keliling_tanah}";
+$volume_bola = volumebola($jari);
+echo "Volume bola budi adalah {$volume_bola}";
+$volume_tabung = volumetabung($jari, $tinggi);
+echo "Volume tabung budi adalah {$volume_tabung}";
+$volume_kerucut = volumekerucut($jari, $tinggi);
+echo "Volume kerucut budi adalah {$volume_kerucut}";
