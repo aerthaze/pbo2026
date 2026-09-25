9dc0741d47a6a004315ba87d9dc812b767002c1f
aerthaze <sampahselokan043@gmail.com>
2026-09-25 22:41:56 +0700

    Praktikum 6F - Multilevel Inheritance

--- public/duabelas.php
+++ public/duabelas.php
@@ -3,11 +3,14 @@ require_once '../vendor/autoload.php';
 
 
 use App\Akademik\Dosen;
+use App\Akademik\Dekan;
 
 
 
 
 $dosen1 = new Dosen(101, "Budi Hartono", "08123456789", "Jl. Cendrawasih No. 10", "198012345");
+$dekan1 = new Dekan(101, "Budi Hartono", "08123456789", "Jl. Cendrawasih No. 10", "198012345");
+$dekan1->mengesahkanSuratIjinMagang();
 
 
 $dosen1->cekIn();
