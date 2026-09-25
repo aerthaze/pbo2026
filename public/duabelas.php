39967e62a7cd058fd968cbac59e9779ad34f3133
aerthaze <sampahselokan043@gmail.com>
2026-09-25 22:32:54 +0700

    Praktikum 6D - Memahami Pewarisan

---
+++ public/duabelas.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../vendor/autoload.php';
+
+
+use App\Akademik\Dosen;
+
+
+
+
+$dosen1 = new Dosen(101, "Budi Hartono", "08123456789", "Jl. Cendrawasih No. 10", "198012345");
+
+
+$dosen1->cekIn();
